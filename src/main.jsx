0607816c4682f9ef475b6ecf298b54a927bbbb03
Laboratório Informática <42labinfo@senacrjedu.local>
2023-01-16 16:50:46 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -30,7 +30,8 @@ const router = createBrowserRouter([
         element: <Adiciona/>
       },
       {
-        path: "/:genero/:id",
+        
+        path: "/terror/:id",
         element: <Filme/>
       },
 

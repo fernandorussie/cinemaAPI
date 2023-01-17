ea381e9f0098d0b91ac64ca5141c92e4bbe23e93
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2023-01-17 16:27:36 -0300

    renderizando filmes a partir de componente Filme

--- src/routes/Terror/index.jsx
+++ src/routes/Terror/index.jsx
@@ -1,16 +1,12 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import Modal from "react-modal"
-import { toast } from'react-toastify'
 import api from '../../service/api'
 
 import 'react-toastify/dist/ReactToastify.css';
 import './styles.css'
+import Filme from '../../components/Filme/Filme';
 
-Modal.setAppElement('#root');
 function App() {
-
-  const [modalIsOpen, setIsOpen] = useState(false);
   const [item, setItem] = useState([]);
   const [load, setLoad] = useState(true);
 
@@ -30,36 +26,6 @@ function App() {
     getApi()
   }, []);
  
-  const deleteElement = async (id) => {
-    // if(window.confirm('Você realmente quer excluir o filme?')){
-    // }
-    await api.delete(`/terror/${id}`)
-    .then((sucesso) => {
-      // navigate("/", {replace: true})
-      console.log(sucesso)
-      toast.success('Item excluido com sucesso!');
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-
-    let filtro = item.filter((item) => {
-      return (item.id !== id)
-    })
-    setItem(filtro);
-    setIsOpen(false)
-    // window.location.reload();
-  }
-
-  function openModal() {
-      setIsOpen(true);
-  }
-
-  function closeModal() {
-      setIsOpen(false);
-      window.location.reload();
-  }
-
   if(load){
     return (
       <div className='container'>
@@ -77,38 +43,7 @@ function App() {
           </div>
           : (
             item.map((item) => (
-              <div key={item.id} className='property-card'>
-                <img src={item.poster} alt="" />
-                <div className='property-description'>
-                  <h5>{item.nome}</h5>
-                  <div className='description'>{item.descricao}</div>
-                  <div className='box-config'>
-                    <Link className='btn-link' to={`/terror/${item.id}`}>
-                      Saiba mais
-                    </Link>
-                    
-                    <button 
-                    className='btn-delete' 
-                    // onClick={openModal}
-                    onClick={() => deleteElement(item.id)}
-                    >
-                      Excluir
-                    </button>
-                  </div>
-                </div>
-                <Modal
-                  isOpen={modalIsOpen}
-                  onRequestClose={closeModal}
-                  contentLabel="Example Modal"
-                  overlayClassName="modal-overlay"
-                  className="modal-content"
-                  key={item.id}
-                >
-                  <h1>Você realmente quer excluir {item.nome} da sua lista de filmes?{item.id}</h1>
-                  <button className='btn-delete' onClick={() => deleteElement(item.id)}>Sim</button>
-                  <button className='btn-delete' onClick={closeModal}>Não</button>
-                </Modal>
-              </div>
+              <Filme id={item.id} poster={item.poster} nome={item.nome} descricao={item.descricao} key={item.id} />
             ))
           )
         }
